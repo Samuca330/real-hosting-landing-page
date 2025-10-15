36bb2fd090b779d20bf492b66e4dddd076e1160e
Samuca330 <samuelrodriguesfarias09@gmail.com>
2025-10-15 15:49:01 -0300

    organizando paginas e arquivos

--- javascript/js.js
+++ javascript/js.js
@@ -2,6 +2,6 @@ const botaocontratar = document.querySelectorAll(".btn-contratar");
 
 botaocontratar.forEach(function(botao) {
     botao.addEventListener("click", function(event) {
-        window.location = "../pagina-planos/planos.html";
+        window.location = "planos.html";
     })
 })
